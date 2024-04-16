a22a72f873b2ca8d240b3d22101590e8e58c7273
le_gabi <legabi.dev@gmail.com>
2024-04-16 18:04:30 +0000

    ça n'a pas fonctionné

--- OVH/AI/main.py
+++ OVH/AI/main.py
@@ -23,17 +23,19 @@ MaxPooling1D = tf.keras.layers.MaxPooling1D
 BatchNormalization = tf.keras.layers.BatchNormalization
 Input = tf.keras.layers.Input
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-print(gpus)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+gpus = tf.config.list_physical_devices('GPU')
 if gpus:
-    tf.config.set_visible_devices([], 'CPU') # hide the CPU
-    tf.config.set_visible_devices(gpus[0], 'GPU') # unhide potentially hidden GPU
-    tf.config.get_visible_devices()
-else: 
-    print("No GPU found")
-    num_proc = os.cpu_count()
-    print(f"Number of processors: {num_proc}")
-    tf.config.threading.set_intra_op_parallelism_threads(num_proc)
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
 
 os.environ["HF_DATASETS_OFFLINE"] = "1"
 
