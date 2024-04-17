5728d8e334ac02b952b40e4cb0ff706e7c9d9405
le_gabi <legabi.dev@gmail.com>
2024-04-17 20:14:14 +0000

    o

--- OVH/AI/main.py
+++ OVH/AI/main.py
@@ -119,7 +119,7 @@ def data_generator(batch_size):
                 yield inputs, labels
 
 # Use the data generator for model training
-batch_size = 300
+batch_size = 200
 model.fit(data_generator(batch_size), steps_per_epoch=len(df) // batch_size, epochs=3, verbose=1)
 
 
