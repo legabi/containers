fe9aa2d8b0f3a310d08f173151282258b0103d43
le_gabi <legabi.dev@gmail.com>
2024-04-18 09:05:05 +0000

    On save un peu plus

--- OVH/AI/main.py
+++ OVH/AI/main.py
@@ -5,6 +5,7 @@ from keras.utils import to_categorical
 from tensorflow.keras.utils import plot_model
 import os
 import pickle
+from tensorflow.keras.callbacks import Callback
 
 Tokenizer = tf.keras.preprocessing.text.Tokenizer
 pad_sequences = tf.keras.preprocessing.sequence.pad_sequences
@@ -90,6 +91,20 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 model.summary()
 
 
+class CustomModelCheckpoint(Callback):
+    def __init__(self, save_freq):
+        super(CustomModelCheckpoint, self).__init__()
+        self.save_freq = save_freq
+        self.batch_counter = 0
+
+    def on_batch_end(self, batch, logs=None):
+        self.batch_counter += 1
+        if self.batch_counter % self.save_freq == 0:
+            self.model.save(f'output/model_at_batch_{self.batch_counter}.h5')
+
+# Instantiate the custom callback
+custom_checkpoint = CustomModelCheckpoint(save_freq=100)
+
 df = load_dataset('PleIAs/French-PD-Books', split='train')
 
 def data_generator(batch_size):
@@ -102,7 +117,7 @@ def data_generator(batch_size):
                 data['inputs'].append(tokenizer.texts_to_sequences([x['title']])[0])
                 data["labels"].append(tokenizer.texts_to_sequences([x['complete_text']])[0])
 
-            seq_len = len(tokenizer.word_index)+1
+            seq_len = max([len(x) for x in data['inputs']])
             data['inputs'] = pad_sequences(data['inputs'], padding='post', maxlen=seq_len)
             data['labels'] = pad_sequences(data['labels'], padding='post', maxlen=seq_len)
 
@@ -120,7 +135,8 @@ def data_generator(batch_size):
 
 # Use the data generator for model training
 batch_size = 200
-model.fit(data_generator(batch_size), steps_per_epoch=len(df) // batch_size, epochs=3, verbose=1)
+# model.fit(data_generator(batch_size), steps_per_epoch=len(df) // batch_size, epochs=3, verbose=1)
+model.fit(data_generator(batch_size), steps_per_epoch=len(df) // batch_size, epochs=3, callbacks=[custom_checkpoint])
 
 
 # %% [markdown]
