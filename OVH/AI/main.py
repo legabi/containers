87805628c8214f9af6f7046de7eab33b8760034c
le_gabi <legabi.dev@gmail.com>
2024-05-08 20:43:03 +0000

    on change ça

--- OVH/AI/main.py
+++ OVH/AI/main.py
@@ -7,8 +7,8 @@ from tensorflow.keras.utils import plot_model
 import os
 import pickle
 from tensorflow.keras.callbacks import Callback
-import tqdm
-
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 # test if i can write in output, else exit
 if not os.path.exists('output'):
@@ -28,6 +28,15 @@ TimeDistributed = tf.keras.layers.TimeDistributed
 LSTM = tf.keras.layers.LSTM
 Dropout = tf.keras.layers.Dropout
 BatchNormalization = tf.keras.layers.BatchNormalization
+Input = tf.keras.layers.Input
+# optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.001, rho=0.9)
+
+optimizer = tf.keras.optimizers.AdamW(
+        learning_rate=5e-5,
+        weight_decay=0.01,
+        epsilon=1e-6,
+        global_clipnorm=1.0,  # Gradient clipping.
+    )
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
@@ -75,17 +84,21 @@ lstm_units = 4096
 
 model = Sequential()
 model.add(Embedding(vocab_size, embedding_dim))
-model.add(LSTM(lstm_units, return_sequences=True))
-model.add(LSTM(lstm_units))
 for _ in range(32):
-    model.add(Dense(4096, activation='relu'))
+    model.add(LSTM(lstm_units, return_sequences=True))
+    model.add(TimeDistributed(Dense(lstm_units, activation='relu')))
     model.add(Dropout(0.2))
-model.add(Dense(512, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.add(BatchNormalization())
+model.add(TimeDistributed(Dense(vocab_size, activation='softmax')))
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
 model.summary()
 
-print(f'The model has {sum(p.numel() for p in model.trainable_variables)} parameters')
+# # build model
+# model.build(input_shape=(None, 512))
+# model.summary()
+
+# plot_model(model, to_file='output/model.png', show_shapes=True)
 
 class CustomModelCheckpoint(Callback):
     def __init__(self, save_freq):
@@ -101,7 +114,7 @@ class CustomModelCheckpoint(Callback):
 # Instantiate the custom callback
 custom_checkpoint = CustomModelCheckpoint(save_freq=100)
 
-df = load_dataset('PleIAs/French-PD-Books', split='train')
+df = load_dataset('PleIAs/French-PD-Books', split='train[:500]')
 
 def data_generator(batch_size):
     while True:
@@ -109,12 +122,20 @@ def data_generator(batch_size):
             model.summary()
             df_batch = load_dataset('PleIAs/French-PD-Books', split=f'train[{i}:{i+batch_size}]')
 
+            # add <|user|> and <|bot|> and <|end|> tokens
+            # df_batch = df_batch.map(lambda x: {'title': f"<|user|>{x['title']}<|bot|>", 'complete_text': f"{x['complete_text']}<|end|>"})
+            df_batch = df_batch.map(lambda x: {'title': f"<|user|>{x['title']}<|bot|>", 'complete_text': f"{x['complete_text']}<|end|>"}, 
+            num_proc=os.cpu_count(), 
+            batched=True)
+
+            print("Batching...")
+
             data = {'inputs': [], 'labels': []}
             for x in df_batch:
-                inputs = tokenizer.encode(x['title'], return_tensors='tf', truncation=True, padding='max_length', max_length=512)
-                labels = tokenizer.encode(x['complete_text'], return_tensors='tf', truncation=True, padding='max_length', max_length=512)
+                inputs = tokenizer.encode(x['title'], return_tensors='tf', truncation=True, padding='max_length', max_length=512, add_special_tokens=True)
+                labels = tokenizer.encode(x['complete_text'], return_tensors='tf', truncation=True, padding='max_length', max_length=512, add_special_tokens=True)
                 data['inputs'].append(inputs)
-                data['labels'].append(labels)
+                data['labels'].append(to_categorical(labels, num_classes=vocab_size))
 
             data['inputs'] = tf.concat(data['inputs'], axis=0)
             data['labels'] = tf.concat(data['labels'], axis=0)
@@ -134,7 +155,14 @@ def data_generator(batch_size):
 # Use the data generator for model training
 batch_size = 100
 # model.fit(data_generator(batch_size), steps_per_epoch=len(df) // batch_size, epochs=3, verbose=1)
-model.fit(data_generator(batch_size), steps_per_epoch=len(df) // batch_size, epochs=3, callbacks=[custom_checkpoint])
+history = model.fit(data_generator(batch_size), steps_per_epoch=len(df) // batch_size, epochs=3, callbacks=[custom_checkpoint])
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['loss'])
+plt.title('Model Training History')
+plt.xlabel('Epoch')
+plt.legend(['Accuracy', 'Loss'], loc='upper left')
+plt.savefig('output/training_history.png')
 
 
 # %% [markdown]
@@ -143,22 +171,12 @@ model.fit(data_generator(batch_size), steps_per_epoch=len(df) // batch_size, epo
 # %%
 # Train the model
 
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
 # Save the model
 model.save('output/model.keras')
 
 # Save the tokenizer
 with open('output/tokenizer.pkl', 'wb') as f:
     pickle.dump(tokenizer, f)
-
-# Test the model
-def predict(text, optimal_length=1000):
-    text = tokenizer.texts_to_sequences([text])
-    text = pad_sequences(text, padding='post')
-    text = model.predict(text)
-    text = np.argmax(text, axis=-1)
-    text = tokenizer.sequences_to_texts(text)
-    text = ' '.join(text)
-    return text[:optimal_length]
-
-# Generate an history
-print(predict("Genere moi une histoire qui parle de : La guerre des mondes"))
