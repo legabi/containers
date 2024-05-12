68b9f83c09b90ec757054bbea327cefbd51fb381
Gabriel BITAUDEAU <legabi.dev@gmail.com>
2024-05-12 17:47:48 +0200

    Update main.py

--- OVH/AI/main.py
+++ OVH/AI/main.py
@@ -3,7 +3,6 @@ import numpy as np
 import os
 os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
-os.environ["HF_DATASETS_OFFLINE"] = "1"
 import tensorflow as tf
 from datasets import load_dataset, Dataset;
 from transformers import BertTokenizer
